cbda553eacaf37a99dbd850491a2c8dd02d7649b
Numan61 <37619174+Numan61@users.noreply.github.com>
2020-04-16 08:35:58 +0200

    version1.0

---
+++ scripts/cities.js
@@ -0,0 +1,52 @@
+var cities = [
+  {
+    name: 'Amsterdam',
+    coordinates: [4.895168, 52.370216]
+  },
+  {
+    name: 'Rotterdam',
+    coordinates: [4.47917, 51.9225]
+  },
+  {
+    name: 'Nijmegen',
+    coordinates: [5.85278, 51.8425]
+  },
+  {
+    name: 'Maastricht',
+    coordinates: [5.68889, 50.84833]
+  },
+  {
+    name: 'Den Haag',
+    coordinates: [4.3006999, 52.0704978]
+  },
+  {
+    name: 'Enschede',
+    coordinates: [6.89583, 52.21833]
+  },
+    {
+    name: 'Madrid',
+    coordinates: [-3.7037902, 40.4167754]
+  },
+     {
+    name: 'Barcelona',
+    coordinates: [2.154007, 41.390205]
+  },
+    
+      {
+    name: 'Seville',
+    coordinates: [-5.994072, 37.392529]
+  },
+    
+        {
+    name: 'Bilbao',
+    coordinates: [-2.935013, 43.262985]
+  },
+
+        {
+    name: 'Valencia',
+    coordinates: [-0.375000, 39.466667]
+  },
+    
+
+    
+];
---
+++ scripts/main.js
@@ -0,0 +1,152 @@
+
+function getAPIdata() {
+
+	var url = 'https://api.openweathermap.org/data/2.5/weather';
+	var apiKey ='4b7c47eb13e7049686e15a7df0ac84ab';
+	var city = document.getElementById('city').value;
+
+	// construct request
+	var request = url + '?' + 'appid=' + apiKey + '&' + 'q=' + city;
+	
+	// get current weather
+	fetch(request)
+	
+	// parse to JSON format
+	.then(function(response) {
+		if(!response.ok) throw Error(response.statusText);
+		return response.json();
+	})
+	
+	// render weather per day
+	.then(function(response) {
+		// render weatherCondition
+		onAPISucces(response);	
+	})
+	
+	// catch error
+	.catch(function (error) {
+		onAPIError(error);
+	});
+}
+
+
+function onAPISucces(response) {
+	// get type of weather in string format
+	var type = response.weather[0].description;
+
+	// get temperature in Celcius
+	var degC = Math.floor(response.main.temp - 273.15);
+
+	// render weather in DOM
+	var weatherBox = document.getElementById('weather');
+	weatherBox.innerHTML = degC + '&#176;C <br>' + type;
+}
+
+
+function onAPIError(error) {
+	console.error('Fetch request failed', error);
+	var weatherBox = document.getElementById('weather');
+	weatherBox.innerHTML = 'No weather data available <br /> Did you enter a valid city?'; 
+}
+
+document.getElementById('getWeather').onclick = function(){
+	// init data stream
+	getAPIdata();
+};
+
+
+
+
+
+// Set api token
+mapboxgl.accessToken = 'pk.eyJ1IjoibnVtYW42MSIsImEiOiJjazhrNTE4cnIwYnByM2ZzbTRsb2cwMTA2In0.WlneZ8e7cd6WoB7jD7fa3A';
+
+// Initiate map
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/outdoors-v11',
+  center: [5.508852, 52.142480],
+  zoom: 7
+});
+
+
+var layerList = document.getElementById('menu');
+var inputs = layerList.getElementsByTagName('input');
+ 
+function switchLayer(layer) {
+var layerId = layer.target.id;
+map.setStyle('mapbox://styles/mapbox/' + layerId);
+}
+ 
+for (var i = 0; i < inputs.length; i++) {
+inputs[i].onclick = switchLayer;
+}
+
+document.getElementById('fly').addEventListener('click', function() {
+// Fly to a random location by offsetting the point -74.50, 40
+// by up to 5 degrees.
+    map.flyTo({
+    center: [
+    -5.85278 + (Math.random() - 0.5) * 10,
+    40 + (Math.random() - 0.5) * 10
+    ],
+    essential: true // this animation is considered essential with respect to prefers-reduced-motion
+    });
+});
+
+var openWeatherMapUrl = 'https://api.openweathermap.org/data/2.5/weather';
+var openWeatherMapUrlApiKey = '6a719e3c4dfb752cbb9fe577d9c14591';
+
+map.on('load', function () {
+  cities.forEach(function(city) {
+    // Usually you do not want to call an api multiple times, but in this case we have to
+    // because the openWeatherMap API does not allow multiple lat lon coords in one request.
+    var request = openWeatherMapUrl + '?' + 'appid=' + openWeatherMapUrlApiKey + '&lon=' + city.coordinates[0] + '&lat=' + city.coordinates[1];
+
+    // Get current weather based on cities' coordinates
+    fetch(request)
+      .then(function(response) {
+        if(!response.ok) throw Error(response.statusText);
+        return response.json();
+      })
+      .then(function(response) {
+        // Then plot the weather response + icon on MapBox
+        plotImageOnMap(response.weather[0].icon, city)
+      })
+      .catch(function (error) {
+        console.log('ERROR:', error);
+      });
+  });
+});
+
+function plotImageOnMap(icon, city) {
+  map.loadImage(
+    'http://openweathermap.org/img/w/' + icon + '.png',
+    function (error, image) {
+      if (error) throw error;
+      map.addImage("weatherIcon_" + city.name, image);
+      map.addSource("point_" + city.name, {
+        type: "geojson",
+        data: {
+          type: "FeatureCollection",
+          features: [{
+            type: "Feature",
+            geometry: {
+              type: "Point",
+              coordinates: city.coordinates
+            }
+          }]
+        }
+      });
+      map.addLayer({
+        id: "points_" + city.name,
+        type: "symbol",
+        source: "point_" + city.name,
+        layout: {
+          "icon-image": "weatherIcon_" + city.name,
+          "icon-size": 1.3
+        }
+      });
+    }
+  );
+}
